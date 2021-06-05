3d38c887a351327bcfd4dd6e7ff3fdd928e138fd
sergeymild <sergeymild@yandex.ru>
2021-06-05 09:05:46 +0300

    change imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,1 @@
-import {requireNativeComponent, ViewStyle} from 'react-native'
-
-type BottomSheetProps = {
-  color: string
-  style: ViewStyle
-}
-
-export const BottomSheetViewManager =
-  requireNativeComponent<BottomSheetProps>('BottomSheetView')
-
-export default BottomSheetViewManager
+export * from './useBottomSheetView'
